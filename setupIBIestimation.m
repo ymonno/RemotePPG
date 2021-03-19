6949f86993583a64ca4adc928ac44c64495f6f6b
Yusuke Monno <53037045+ymonno@users.noreply.github.com>
2021-03-19 20:11:44 +0900

    Update setupIBIestimation.m

--- setupIBIestimation.m
+++ setupIBIestimation.m
@@ -6,13 +6,13 @@ addpath(genpath(installDirec))
 
 % install FastICA
 disp('Installing FastICA...')
-urlFastICA = 'https://research.ics.aalto.fi/ica/fastica/code/FastICA_2.5.zip';
+urlFastICA = 'http://research.ics.aalto.fi/ica/fastica/code/FastICA_2.5.zip';
 filenameFastICA = '.';
 unzip(urlFastICA,filenameFastICA)
 
 % install facial landmark detection
 disp('Installing find_face_landmarks...')
-urlFind_dace_landmark = 'https://github.com/YuvalNirkin/find_face_landmarks/releases/download/1.2/find_face_landmarks-1.2-x64-vc14-release.zip';
+urlFind_dace_landmark = 'http://github.com/YuvalNirkin/find_face_landmarks/releases/download/1.2/find_face_landmarks-1.2-x64-vc14-release.zip';
 filenameFind_face_landmark = 'find_face_landmark';
 websave(filenameFind_face_landmark, urlFind_dace_landmark);
 unzip(strcat(filenameFind_face_landmark,'.zip'),filenameFind_face_landmark)
